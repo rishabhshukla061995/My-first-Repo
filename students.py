18ed5d5c4cd31347cfc621b9b535e04109ec63ae
rishabhshukla061995 <110488525+rishabhshukla061995@users.noreply.github.com>
2022-08-05 22:37:08 +0530

    Add files via upload

---
+++ students.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[3]:
+
+
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+pd.read_csv(r"C:\Users\KISHAN\Downloads\data.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
